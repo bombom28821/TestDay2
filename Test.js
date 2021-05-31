3a25778588481d2af846f8509c2f2474ff4c5eb7
bombom28821 <bombom28821@gmail.com>
2021-05-31 17:19:57 +0700

    first commit

---
+++ Test.js
@@ -0,0 +1,30 @@
+// const express = require('express')
+
+// const app = express()
+
+// app.get('/',(req,res)=>{
+//     res.sendFile(__dirname + '/public/index.html');
+// })
+
+// app.get('/about', (req,res)=>{
+//     res.sendFile(__dirname + '/public/about.html');
+// })
+
+// const PORT = 5000;
+// app.listen(process.env.PORT || PORT);
+// console.log('Sever is running')
+
+const express = require('express');
+
+const app = express()
+
+app.get('/index.html',(req,res)=>{
+    res.sendFile(__dirname + '/index.html');
+})
+app.get('/huy.html',(req,res)=>{
+    res.sendFile(__dirname + '/huy.html');
+})
+
+const PORT = 5000;
+app.listen(process.env.PORT || PORT);
+console.log("Sever is running");
